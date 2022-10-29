dd4de719d82d0419a95ed6f4d7dc05f3c0fd201d
Todd Twiggs <toddtwiggs@gmail.com>
2022-10-29 17:05:36 -0400

    Added templates, webpack encore

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,22 +2,58 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class AccountController
+class AccountController extends AbstractController
 {
-    #[Route('/accounts/{slug}')]
-    public function accounts(?string $slug = null): Response
+    #[Route('/accounts')]
+    public function index(): Response
     {
-        if ($slug) {
-            $title = 'Account: ' . u(str_replace('-', ' ', $slug))->title(true)->__toString();
-        }
-        else {
-            $title = 'All Accounts';
-        }
+        $accounts = [
+            [
+                'name' => 'Todd\'s Roth IRA',
+                'institution' => 'Fidelity Investments',
+                'owner' => 'Todd Twiggs',
+                'account_number' => '239147938',
+                'total' => '$20,788.94'
+            ],
+            [
+                'Name' => 'Emily\'s Roth IRA',
+                'institution' => 'Fidelity Investments',
+                'owner' => 'Emily Twiggs',
+                'account_number' => '233720188',
+                'total' => '$11,555.71'
+            ],
+            [
+                'name' => 'QEC 401k',
+                'institution' => 'Fidelity Investments',
+                'owner' => 'Emily Twiggs',
+                'account_number' => '70377',
+                'total' => '$835.45'
+            ],
+            [
+                'name' => 'Firebrand 401k',
+                'institution' => 'Transamerica',
+                'owner' => 'Todd Twiggs',
+                'account_number' => '801461-00000',
+                'total' => '$1,199.37'
+            ],
+        ];
 
-        return new Response($title, 200);
+        return $this->render('account/index.html.twig', [
+            'title' => 'Accounts',
+            'accounts' => $accounts
+        ]);
+    }
+
+    #[Route('/accounts/{slug}')]
+    public function show(string $slug): Response
+    {
+        return $this->render('account/index.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug))
+        ]);
     }
 }
